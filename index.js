2ecd84eae91f314195c44d25c404584bd9a53626
jwaddell10 <jewaddell91@gmail.com>
2023-06-15 14:27:06 -0500

    eslint

--- index.js
+++ index.js
@@ -1,80 +1,82 @@
-const $form = document.querySelector("form").addEventListener("submit", (e) => {
-    e.preventDefault();
-   addBookToLibrary();
-  });
+/* eslint-disable no-useless-concat */
+/* eslint-disable no-plusplus */
+/* eslint-disable func-names */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-use-before-define */
+const $form = document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  addBookToLibrary();
+});
 
+// pops-up form that gets book information from user
 
-//pops-up form that gets book information from user
+const userInputButton = document.querySelector('.add-book');
+userInputButton.addEventListener('click', () => {
+  document.getElementById('popUpForm').style.display = 'block';
+});
 
-const userInputButton = document.querySelector('.add-book')
-userInputButton.addEventListener("click", function() {
-    document.getElementById('popUpForm').style.display = "block";
-})
+myLibrary = [];
 
-  myLibrary = [];
-
-  //instantiate Book Object
-  class Book {
-    constructor(title, author, pages, read) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
-    }
+// instantiate Book Object
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
   }
+}
 
-  function addBookToLibrary() {
-    let libraryBook = document.querySelector('#Library-container')
-    let title = document.getElementById('title').value;
-    let author = document.getElementById('author').value;
-    let pages = document.getElementById('pages').value;
-    let read = document.getElementById('readOption').checked;
-    
-        let newBook = new Book(title, author, pages, read);
+function addBookToLibrary() {
+  const libraryBook = document.querySelector('#Library-container');
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const read = document.getElementById('readOption').checked;
 
-    myLibrary.push(newBook);
-    render();
-    }
+  const newBook = new Book(title, author, pages, read);
 
- //render books onto html page
- 
-  function render() {
-    let libraryBook = document.querySelector('#Library-container')
-    libraryBook.innerHTML = "";
+  myLibrary.push(newBook);
+  render();
+}
 
-    for (let i = 0; i < myLibrary.length; i++) {
-    let book = myLibrary[i];
-    let bookEl = document.createElement('div');
-    bookEl.innerHTML = 
-        `<div class="card-header">
+// render books onto html page
+
+function render() {
+  const libraryBook = document.querySelector('#Library-container');
+  libraryBook.innerHTML = '';
+
+  for (let i = 0; i < myLibrary.length; i++) {
+    const book = myLibrary[i];
+    const bookEl = document.createElement('div');
+    bookEl.innerHTML = `<div class="card-header">
         <h3 class="title">${book.title}</h3>
         <h5 class="author">${book.author}</h5>
         <h5 class="pages">${book.pages}` + ` pages</h5>
-        <h5 class="read">${book.read ? "Read" : "Not read yet"}</h5>
+        <h5 class="read">${book.read ? 'Read' : 'Not read yet'}</h5>
         <button class="remove-Btn" onClick="removeBook(${i})">Remove</button>
         <button class="toggle-read-btn" onClick="toggleRead(${i})">Read</button>`;
     libraryBook.appendChild(bookEl);
-    }
   }
+}
 
-//function to remove book from html list
+// function to remove book from html list
 
-  function removeBook(index) {
-    myLibrary.splice(index, 1);
-    render();
-  }
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+  render();
+}
 
-  removeBook()
+removeBook();
 
+// function to change if book was read or not
 
+Book.prototype.toggleRead = function () {
+  this.read = !this.read;
+};
 
-//function to change if book was read or not
-
-Book.prototype.toggleRead = function() {
-    this.read = !this.read;
-  }
-
-  function toggleRead(index) {
-myLibrary[index].toggleRead();
-render();
-  }
+function toggleRead(index) {
+  myLibrary[index].toggleRead();
+  render();
+}
